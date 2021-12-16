326252a363106bf8768744a2323bf7b6c7b82fd3
freshman-git <68904944+freshman-git@users.noreply.github.com>
2021-12-16 22:59:17 +0800

    Create left.java

---
+++ src/java/left/left.java
@@ -0,0 +1,10 @@
+package left;
+
+public class left {
+    public static void main(String[] args) {
+        new left().print();
+    }
+    public void  print(){
+        System.out.println("left");
+    }
+}
