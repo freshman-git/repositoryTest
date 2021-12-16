5dc85b2976554798e33e71c78a50dcc27f513541
freshman-git <68904944+freshman-git@users.noreply.github.com>
2021-12-16 22:59:08 +0800

    Create right.java

---
+++ src/java/right/right.java
@@ -0,0 +1,10 @@
+package right;
+
+public class right {
+    public static void main(String[] args) {
+        new right().print();
+    }
+    public void  print(){
+        System.out.println("left");
+    }
+}
