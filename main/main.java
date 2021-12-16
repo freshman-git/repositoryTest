d85664903fbf005109af55ebb39ff394a7cd7e35
freshman-git <68904944+freshman-git@users.noreply.github.com>
2021-12-16 22:18:12 +0800

    Create main.java

---
+++ main/main.java
@@ -0,0 +1,16 @@
+class hello_main{
+
+	public static void main(String[] args){
+		function();
+		print();
+	}
+	public static void print(){
+		System.out.println("main");
+	}
+
+	public static void function(){
+		System.out.println("this is hello_main.java");
+		System.out.println("world");
+	}
+
+}
